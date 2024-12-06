66586604144337888d0ad180189afa9374eb1b1c
m-ancev <m.ancev04@gmail.com>
2024-12-06 11:26:50 +0100

    refactor nvim treesitter

--- config/nvim/lua/ma/plugins/nvim-treesitter.lua
+++ config/nvim/lua/ma/plugins/nvim-treesitter.lua
@@ -1,20 +1,24 @@
--- Highlight, edit, and navigate code
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
-		-- Autoinstall languages that are not installed
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-	},
-	config = function(_, opts)
-		-- Prefer git instead of curl in order to improve connectivity in some environments
-		require("nvim-treesitter.install").prefer_git = true
-		require("nvim-treesitter.install").compilers = { "clang", "gcc" }
-		---@diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+	config = function()
+		require'nvim-treesitter.configs'.setup {
+			auto_install = true,
+			highlight = {
+				enable = true,
+				disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+								return true
+						end
+				end,
+				additional_vim_regex_highlighting = false,
+			},
+			require("nvim-treesitter.install").prefer_git = true
+			require("nvim-treesitter.install").compilers = { "clang", "gcc" }
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup(opts)
+		}
+	end
 }
