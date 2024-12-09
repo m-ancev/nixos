59e69b884f8a0e61aeb4f4e962a5eee78dc35db1
m-ancev <m.ancev04@gmail.com>
2024-12-09 01:38:26 +0100

    add format on save

--- common/nvim/lua/ma/plugins/lsp.lua
+++ common/nvim/lua/ma/plugins/lsp.lua
@@ -1,19 +1,36 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"folke/lazydev.nvim",
-				ft = "lua",
-				opts = {
-					library = {
-						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-					},
-				},
-			},
-		},
-		config = function()
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
       require("lspconfig").lua_ls.setup {}
-		end,
-	}
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
+
+          -- format on save
+          -- if client.supports_method('textDocument/formatting') then
+          --   vim.api.nvim_create_autocmd('BufWritePre', {
+          --     buffer = args.buf,
+          --     callback = function()
+          --       vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+          --     end,
+          --   })
+          -- end
+        end,
+      })
+    end,
+  }
 }
