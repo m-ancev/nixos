c964c5a86a71ee3b766cd76738568b9eee7b634d
m-ancev <m.ancev04@gmail.com>
2024-12-09 01:38:08 +0100

    add which key plugin

---
+++ common/nvim/lua/ma/plugins/which-key.lua
@@ -0,0 +1,37 @@
+return {
+	"folke/which-key.nvim",
+	event = "VimEnter", -- Sets the loading event to 'VimEnter'
+	config = function() -- This is the function that runs, AFTER loading
+		require("which-key").setup()
+
+		-- Document existing key chains
+		require("which-key").add({
+			{ "<leader>c", group = "[C]ode" },
+			{ "<leader>c_", hidden = true },
+
+			{ "<leader>d", group = "[D]ocument" },
+			{ "<leader>d_", hidden = true },
+
+			{ "<leader>r", group = "[R]ename" },
+			{ "<leader>r_", hidden = true },
+
+			{ "<leader>s", group = "[S]earch" },
+			{ "<leader>s_", hidden = true },
+
+			{ "<leader>w", group = "[W]orkspace" },
+			{ "<leader>w_", hidden = true },
+
+			{ "<leader>t", group = "[T]oggle" },
+			{ "<leader>t_", hidden = true },
+
+			{ "<leader>T", group = "[T]heme" },
+			{ "<leader>T_", hidden = true },
+
+			{ "<leader>h", group = "Git [H]unk" },
+			{ "<leader>h_", hidden = true },
+
+			{ "<leader>n", group = "[N]o" },
+			{ "<leader>n_", hidden = true },
+		})
+	end,
+}
