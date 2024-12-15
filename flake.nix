02e1f17ae68a4a2ac3a431d98ae9f71c3a42c2b1
m-ancev <m.ancev04@gmail.com>
2024-12-15 02:12:43 +0100

    version lock virtualbox on laptop

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    virtualboxPkgs.url = "github:NixOS/nixpkgs/e4cef11b7889c3dd0ff200d8467f16c993f8b5e7";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+  outputs = { self, nixpkgs, home-manager, virtualboxPkgs, ... }@inputs: 
   {
     nixosConfigurations = {
       # Configuration for Tower
@@ -26,6 +27,14 @@
         modules = [
           ./laptop/hardware-configuration.nix
           ./laptop/configuration.nix
+          {
+            # Override the VirtualBox package used by the module
+            nixpkgs.overlays = [
+              (self: super: {
+                virtualbox = virtualboxPkgs.legacyPackages.x86_64-linux.virtualbox;
+              })
+            ];
+          }
         ];
         specialArgs = { inherit inputs; };
       };
