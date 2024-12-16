4253c56d2a7289e7cbf3bb0ffcaec85cceedd994
m-ancev <m.ancev04@gmail.com>
2024-12-16 01:01:01 +0100

    format nvim configuration

--- common/nvim/lua/ma/plugins/oil.lua
+++ common/nvim/lua/ma/plugins/oil.lua
@@ -1,15 +1,15 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {
-		view_options = {
-			show_hidden = true,
-		},
-		float = {
-			padding = 0,
-			max_width = 80,
-			max_height = 20,
-			border = "solid",
-		},
-	},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+  "stevearc/oil.nvim",
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 0,
+      max_width = 80,
+      max_height = 20,
+      border = "solid",
+    },
+  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 }
--- common/nvim/lua/ma/plugins/which-key.lua
+++ common/nvim/lua/ma/plugins/which-key.lua
@@ -1,34 +1,34 @@
 return {
-	"folke/which-key.nvim",
-	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	config = function() -- This is the function that runs, AFTER loading
-		require("which-key").setup()
+  "folke/which-key.nvim",
+  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require("which-key").setup()
 
-		-- Document existing key chains
-		require("which-key").add({
-			{ "<leader>c", group = "[C]ode" },
-			{ "<leader>c_", hidden = true },
+    -- Document existing key chains
+    require("which-key").add({
+      { "<leader>c",  group = "[C]ode" },
+      { "<leader>c_", hidden = true },
 
-			{ "<leader>d", group = "[D]ocument" },
-			{ "<leader>d_", hidden = true },
+      { "<leader>d",  group = "[D]ocument" },
+      { "<leader>d_", hidden = true },
 
-			{ "<leader>r", group = "[R]ename" },
-			{ "<leader>r_", hidden = true },
+      { "<leader>r",  group = "[R]ename" },
+      { "<leader>r_", hidden = true },
 
-			{ "<leader>s", group = "[S]earch" },
-			{ "<leader>s_", hidden = true },
+      { "<leader>s",  group = "[S]earch" },
+      { "<leader>s_", hidden = true },
 
-			{ "<leader>w", group = "[W]orkspace" },
-			{ "<leader>w_", hidden = true },
+      { "<leader>w",  group = "[W]orkspace" },
+      { "<leader>w_", hidden = true },
 
-			{ "<leader>t", group = "[T]oggle" },
-			{ "<leader>t_", hidden = true },
+      { "<leader>t",  group = "[T]oggle" },
+      { "<leader>t_", hidden = true },
 
-			{ "<leader>T", group = "[T]heme" },
-			{ "<leader>T_", hidden = true },
+      { "<leader>T",  group = "[T]heme" },
+      { "<leader>T_", hidden = true },
 
-			{ "<leader>n", group = "[N]o" },
-			{ "<leader>n_", hidden = true },
-		})
-	end,
+      { "<leader>n",  group = "[N]o" },
+      { "<leader>n_", hidden = true },
+    })
+  end,
 }
