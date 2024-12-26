29f7334bc11b6a40d79dff448923402e471b1ed1
m-ancev <m.ancev04@gmail.com>
2024-12-26 02:50:55 +0100

    rename highlight yank autocmd

--- common/nvim/lua/ma/core/options.lua
+++ common/nvim/lua/ma/core/options.lua
@@ -76,7 +76,7 @@ vim.opt.scrolloff = 10
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
