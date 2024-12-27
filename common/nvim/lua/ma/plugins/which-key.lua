3246e195132c82925cea4227b3d35f4bcd5d26d7
m-ancev <m.ancev04@gmail.com>
2024-12-27 03:53:03 +0100

    remove redundant key chain definitions in which key

--- common/nvim/lua/ma/plugins/which-key.lua
+++ common/nvim/lua/ma/plugins/which-key.lua
@@ -6,29 +6,11 @@ return {
 
     -- Document existing key chains
     require("which-key").add({
-      { "<leader>c",  group = "[C]ode" },
-      { "<leader>c_", hidden = true },
-
-      { "<leader>d",  group = "[D]ocument" },
-      { "<leader>d_", hidden = true },
-
-      { "<leader>r",  group = "[R]ename" },
-      { "<leader>r_", hidden = true },
-
       { "<leader>s",  group = "[S]earch" },
       { "<leader>s_", hidden = true },
 
-      { "<leader>w",  group = "[W]orkspace" },
-      { "<leader>w_", hidden = true },
-
-      { "<leader>t",  group = "[T]oggle" },
-      { "<leader>t_", hidden = true },
-
       { "<leader>T",  group = "[T]heme" },
       { "<leader>T_", hidden = true },
-
-      { "<leader>n",  group = "[N]o" },
-      { "<leader>n_", hidden = true },
     })
   end,
 }
