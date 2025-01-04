55b98eff0132b6aee5f014dd4082647ffe950450
m-ancev <m.ancev04@gmail.com>
2025-01-04 18:25:49 +0100

    disable bufferline nvim

--- common/nvim/lua/ma/plugins/bufferline.lua
+++ common/nvim/lua/ma/plugins/bufferline.lua
@@ -1,20 +1,20 @@
 return {
-  "akinsho/bufferline.nvim",
-  config = function()
-    local bufferline = require("bufferline")
-    bufferline.setup({
-      options = {
-        style_preset = {
-          bufferline.style_preset.no_italic,
-          bufferline.style_preset.no_bold,
-        },
-        indicator = {
-          style = "none",
-        },
-        show_buffer_close_icons = false,
-      },
-    })
-  end,
-  version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
+  -- "akinsho/bufferline.nvim",
+  -- config = function()
+  --   local bufferline = require("bufferline")
+  --   bufferline.setup({
+  --     options = {
+  --       style_preset = {
+  --         bufferline.style_preset.no_italic,
+  --         bufferline.style_preset.no_bold,
+  --       },
+  --       indicator = {
+  --         style = "none",
+  --       },
+  --       show_buffer_close_icons = false,
+  --     },
+  --   })
+  -- end,
+  -- version = "*",
+  -- dependencies = "nvim-tree/nvim-web-devicons",
 }
