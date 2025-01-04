43ef4969865e8d9d486b1116bf408f701e130e86
m-ancev <m.ancev04@gmail.com>
2025-01-04 20:39:57 +0100

    refactor lsp and completion nvim

--- common/nvim/lua/ma/plugins/lsp.lua
+++ common/nvim/lua/ma/plugins/lsp.lua
@@ -1,6 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = { 'saghen/blink.cmp',
+  dependencies = {
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'rafamadriz/friendly-snippets',
+    'saghen/blink.cmp',
     {
       "folke/lazydev.nvim",
       ft = "lua",
@@ -16,15 +22,17 @@ return {
       "lua_ls",
       "nil_ls",
       "jedi_language_server",
-      -- add more servers here
     }
 
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    -- Capabilities for LSP servers
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+    -- Set up LSP servers
     for _, lsp in ipairs(servers) do
       require("lspconfig")[lsp].setup({ capabilities = capabilities })
     end
 
+    -- Auto commands for LSP
     vim.api.nvim_create_autocmd('LspAttach', {
       callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
@@ -33,40 +41,28 @@ return {
         local bufnr = args.buf
         local opts = { buffer = bufnr, remap = false }
 
-        -- Keybindings
-        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end,
-          vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Reference" }))
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end,
-          vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Definition" }))
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end,
-          vim.tbl_deep_extend("force", opts, { desc = "LSP Hover" }))
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end,
-          vim.tbl_deep_extend("force", opts, { desc = "LSP Workspace Symbol" }))
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.setloclist() end,
-          vim.tbl_deep_extend("force", opts, { desc = "LSP Show Diagnostics" }))
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end,
-          vim.tbl_deep_extend("force", opts, { desc = "Next Diagnostic" }))
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end,
-          vim.tbl_deep_extend("force", opts, { desc = "Previous Diagnostic" }))
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end,
-          vim.tbl_deep_extend("force", opts, { desc = "LSP Code Action" }))
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end,
-          vim.tbl_deep_extend("force", opts, { desc = "LSP References" }))
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end,
-          vim.tbl_deep_extend("force", opts, { desc = "LSP Rename" }))
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end,
-          vim.tbl_deep_extend("force", opts, { desc = "LSP Signature Help" }))
-
-        -- Uncomment the following block for format on save functionality
-        -- if client.supports_method('textDocument/formatting') then
-        --   vim.api.nvim_create_autocmd('BufWritePre', {
-        --     buffer = bufnr,
-        --     callback = function()
-        --       vim.lsp.buf.format({ bufnr = bufnr, id = client.id })
-        --     end,
-        --   })
-        -- end
+        -- LSP keybindings
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
       end,
+    })
+
+    -- Set up nvim-cmp
+    require('cmp').setup({
+      sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+      },
+      mapping = {
+        ['<C-n>'] = require('cmp').mapping.select_next_item(),
+        ['<C-p>'] = require('cmp').mapping.select_prev_item(),
+        ['<Tab>'] = require('cmp').mapping.confirm({ select = true }),
+      },
     })
   end,
 }
