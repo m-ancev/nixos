a631cd1a7d75ea1d8ee20fca78c313c082852401
m-ancev <m.ancev04@gmail.com>
2025-01-05 12:42:05 +0100

    refactor completion to include snippets nvim

--- common/nvim/lua/ma/plugins/lsp.lua
+++ common/nvim/lua/ma/plugins/lsp.lua
@@ -6,7 +6,8 @@ return {
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'rafamadriz/friendly-snippets',
-    'saghen/blink.cmp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
     {
       "folke/lazydev.nvim",
       ft = "lua",
@@ -27,13 +28,13 @@ return {
     -- Capabilities for LSP servers
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-    -- Set up LSP servers
+    -- set up lsp servers
     for _, lsp in ipairs(servers) do
       require("lspconfig")[lsp].setup({ capabilities = capabilities })
     end
 
-    -- Auto commands for LSP
-    vim.api.nvim_create_autocmd('LspAttach', {
+    -- auto commands for lsp
+    vim.api.nvim_create_autocmd('lspattach', {
       callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         if not client then return end
@@ -41,27 +42,43 @@ return {
         local bufnr = args.buf
         local opts = { buffer = bufnr, remap = false }
 
-        -- LSP keybindings
+        -- lsp keybindings
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("i", "<c-h>", vim.lsp.buf.signature_help, opts)
       end,
     })
 
-    -- Set up nvim-cmp
-    require('cmp').setup({
+    -- set up nvim-cmp
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
       sources = {
         { name = 'nvim_lsp' },
+        { name = 'luasnip' },
         { name = 'buffer' },
         { name = 'path' },
       },
       mapping = {
-        ['<C-n>'] = require('cmp').mapping.select_next_item(),
-        ['<C-p>'] = require('cmp').mapping.select_prev_item(),
-        ['<Tab>'] = require('cmp').mapping.confirm({ select = true }),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
       },
     })
   end,
