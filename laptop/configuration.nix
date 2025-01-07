b4a34511e6d2ad7feab96c53abd5cb7fc67138f3
m-ancev <m.ancev04@gmail.com>
2025-01-07 09:40:30 +0100

    add window rule to fix screen sharing for xwayland

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -14,6 +14,7 @@ in
   virtualisation.virtualbox.host.enableExtensionPack = true;
 
   environment.systemPackages = with pkgs; [
+      kdePackages.xwaylandvideobridge
       clipse
       hyprpaper
       nwg-look
