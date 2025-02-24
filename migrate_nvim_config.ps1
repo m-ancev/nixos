beec83784dbd678a7b9257c814474948a199ce17
Marko Ancev <97703085+m-ancev@users.noreply.github.com>
2025-02-24 08:32:39 +0100

    Update migrate_nvim_config.ps1

--- migrate_nvim_config.ps1
+++ migrate_nvim_config.ps1
@@ -1,21 +1,26 @@
-
 # Define the source and destination directories
 $sourcePath = Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\nixos\common\nvim"
 $destinationPath = Join-Path -Path $env:USERPROFILE -ChildPath "AppData\Local\nvim"
 $oldDataPath = Join-Path -Path $env:USERPROFILE -ChildPath "AppData\Local\nvim-data"
 
-# Remove all contents from the destination directory and old data path
+# Remove all contents from the destination directory
 Write-Host "deleting files in destination path: $destinationPath ..."
 Remove-Item -Path "$destinationPath\*" -Recurse -Force
-Write-Host "done"
+Write-Host "done`n"
 
-Write-Host "deleting files in data path: $oldDataPath ..."
-Remove-Item -Path "$oldDataPath\*" -Recurse -Force
-Write-Host "done"
+
+$confirmation = Read-Host "are you sure you want to delete files in data path $oldDataPath? (y/n)"
+if ($confirmation -match '^[Yy]$') {
+    Write-Host "deleting files in data path: $oldDataPath ..."
+    Remove-Item -Path "$oldDataPath\*" -Recurse -Force
+    Write-Host "done`n"
+} else {
+    Write-Host "data will persist`n"
+}
 
 # Copy all contents from the source directory to the destination directory
 Write-Host "copying files from source path: $sourcePath into destination path: $destinationPath ..."
 Copy-Item -Path "$sourcePath\*" -Destination $destinationPath -Recurse -Force
-Write-Host "done"
+Write-Host "done`n"
 
-Write-Host "migration complete"
+Write-Host "migration complete`n"
