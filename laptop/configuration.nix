1ae6e81d65a7e478cd3a34fd308f60583427e137
m-ancev <m.ancev04@gmail.com>
2025-03-15 20:19:16 +0100

    add obsidian nix

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -14,6 +14,7 @@ in
   virtualisation.virtualbox.host.enableExtensionPack = true;
 
   environment.systemPackages = with pkgs; [
+      obsidian # notes
       bluetui # bluetooth tui
       brightnessctl
       cbonsai # terminal bonsai tree
