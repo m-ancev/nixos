b29c04e0166c76c85851c9ac1a10f507e28042ed
m-ancev <m.ancev04@gmail.com>
2025-03-15 17:03:14 +0100

    add bluetui nix

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -14,6 +14,7 @@ in
   virtualisation.virtualbox.host.enableExtensionPack = true;
 
   environment.systemPackages = with pkgs; [
+      bluetui # bluetooth tui
       brightnessctl
       cbonsai # terminal bonsai tree
       angular-language-server # angularls
