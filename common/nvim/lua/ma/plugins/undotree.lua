4ead7cb0b34a6fe1416222128281ea0a637ab810
m-ancev <m.ancev04@gmail.com>
2025-03-20 18:07:59 +0100

    add undotree diff command 'fc' for windows

--- common/nvim/lua/ma/plugins/undotree.lua
+++ common/nvim/lua/ma/plugins/undotree.lua
@@ -3,5 +3,7 @@ return {
   "mbbill/undotree",
   config = function()
     vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<CR>", { desc = "[U]ndotree Toggle" })
+    -- hard dependency for windows
+    -- vim.g.undotree_DiffCommand = "FC"
   end
 }
