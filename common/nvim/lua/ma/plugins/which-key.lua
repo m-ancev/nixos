17cd5b063b209b1a0e56551c0305e0c3b07ce894
m-ancev <m.ancev04@gmail.com>
2025-04-09 19:23:30 +0200

    hide icons which key nvim

--- common/nvim/lua/ma/plugins/which-key.lua
+++ common/nvim/lua/ma/plugins/which-key.lua
@@ -1,19 +1,26 @@
 return {
-  "folke/which-key.nvim",
-  event = "VimEnter", -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require("which-key").setup()
+    "folke/which-key.nvim",
+    event = "VimEnter", -- Sets the loading event to 'VimEnter'
+    config = function() -- This is the function that runs, AFTER loading
+        require("which-key").setup({
+            icons = {
+                mappings = false,
+                breadcrumb = "", -- This removes the breadcrumb symbol (usually shows the current key combo path)
+                separator = "", -- This removes the separator (default is something like "➜")
+                group = "", -- This removes the group indicator symbol
+            },
+        })
 
-    -- Document existing key chains
-    require("which-key").add({
-      { "<leader>s",  group = "[S]earch" },
-      { "<leader>s_", hidden = true },
+        -- Document existing key chains
+        require("which-key").add({
+            { "<leader>s",  group = "[S]earch" },
+            { "<leader>s_", hidden = true },
 
-      { "<leader>T",  group = "[T]heme" },
-      { "<leader>T_", hidden = true },
+            { "<leader>T",  group = "[T]heme" },
+            { "<leader>T_", hidden = true },
 
-      { "<leader>v",  group = "LSP" },
-      { "<leader>v_", hidden = true },
-    })
-  end,
+            { "<leader>v",  group = "LSP" },
+            { "<leader>v_", hidden = true },
+        })
+    end,
 }
