d920dd268aa03a066c815cd4c7723fdf02713727
m-ancev <m.ancev04@gmail.com>
2025-04-19 15:11:07 +0200

    fix persistence plugin nvim
    dir error on wsl

--- common/nvim/lua/ma/plugins/persistence.lua
+++ common/nvim/lua/ma/plugins/persistence.lua
@@ -1,4 +1,13 @@
 return {
-    "folke/persistence.nvim",
-    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+  "folke/persistence.nvim",
+  event = "BufReadPre",  -- start it when you actually open a buffer
+  config = function()
+    require("persistence").setup({
+      -- store sessions under Neovim’s state directory in a "sessions" subfolder:
+      dir = vim.fn.stdpath("state") .. "/sessions/",
+      -- which things to store in the session file:
+      options = { "buffers", "curdir", "tabpages", "globals" },
+      -- (you can add autosave, commands, etc. here)
+    })
+  end,
 }
